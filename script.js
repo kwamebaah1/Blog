e336b48b6920da99baa8c82409612dd30c37809f
kwamebaah1 <baahhprince79@gmail.com>
2024-02-07 17:09:31 -0800

    First commit

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.getElementById('bookUploadForm').addEventListener('submit', async (even
     formData.append('image', document.getElementById('bookImage').files[0]);
 
     try {
-        const response = await fetch('/upload', {
+        const response = await fetch('https://gleaming-cuchufli-b12691.netlify.app/books', {
             method: 'POST',
             body: formData
         });
